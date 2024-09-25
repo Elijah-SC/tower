8b99424967923f8d84f4fc8c8b2d7960d3bdc6a1
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-09-25 17:02:10 -0600

    some styling on the event Page.

--- client/src/pages/EventPage.vue
+++ client/src/pages/EventPage.vue
@@ -7,10 +7,18 @@ import { computed, watch } from "vue";
 import { useRoute } from "vue-router";
 
 const activeEvent = computed(() => AppState.activeEvent)
+const account = computed(() => AppState.account)
 const route = useRoute()
 watch(() => route.params.eventId, () => {
   findEventById()
 }, { immediate: true })
+
+const isCreator = computed(() => {
+  if (!account.value) return false
+  if (!activeEvent.value) return false
+  if (account.value.id != activeEvent.value.creatorId) return false
+  return true
+})
 
 async function findEventById() {
   try {
@@ -27,11 +35,29 @@ async function findEventById() {
 
 <template>
 
-  <body>
-    <h1> this is the event Page</h1>
-
+  <body v-if="activeEvent" class="container-fluid">
+    <div class="row justify-content-around">
+      <div class="col-10 p-0">
+        <div class="my-2">
+          <img :src="activeEvent.coverImg" alt="Event CoverImg" class="img-fluid w-100 cover-img">
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center gap-2">
+      <div class="col-8 d-flex justify-content-between">
+        <h1>{{ activeEvent.name }} <span v-html="activeEvent.typeIcon"></span></h1>
+        <button v-if="isCreator && !activeEvent.isCanceled" class="btn btn-outline-dark">Cancel Event</button>
+        <button v-else-if="isCreator && activeEvent.isCanceled" class="btn btn-outline-dark">unCancel Event</button>
+      </div>
+      <div class="col-2"></div>
+    </div>
   </body>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cover-img {
+  min-height: 30vh;
+  border-radius: 5%;
+}
+</style>
